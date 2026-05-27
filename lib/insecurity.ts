6d31dace4fffd5fb91fe6c34f2d01751e96cc99a
Luis Pastor <82288606+luisepastor@users.noreply.github.com>
2026-05-27 10:15:14 -0500

    Update insecurity.ts

--- lib/insecurity.ts
+++ lib/insecurity.ts
@@ -14,6 +14,7 @@ import sanitizeHtmlLib from 'sanitize-html'
 import sanitizeFilenameLib from 'sanitize-filename'
 import * as utils from './utils'
 
+
 /* jslint node: true */
 
 // @ts-expect-error FIXME no typescript definitions for z85 :(
